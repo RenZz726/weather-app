90231cd19af178930162284e7fe65111e9c37ee7
RenZz <rensonkroji02@gmail.com>
2025-09-25 14:54:54 +0530

    first phase completed

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { handleWeather } from "./weather.js";
 import { handleHistorical } from "./historical.js";
 import { handleCity } from "./city.js";
+import { handleAuth } from "./authentication.js";
 
 const router = Router();
 
   router.use("/weather", handleWeather);
   router.use("/historical-weather", handleHistorical);
-  router.use("/city", handleCity); 
+  router.use("/city", handleCity);
+  // router.use("/auth", handleAuth); 
 
 export default router;
--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-// import http from "http";
-import express from "express";
-import cors from "cors";
-import routes from "./routes/index.js";
+import app from "./app.js"
 
-const app = express();
 const PORT = 5000;
 
-app.use(express.json());
-
-app.use(cors());
-
-  app.use("/", routes);
-
-
-  app.use((req, res) => {
-    res.status(404).json({ error: "Endpoint not found"});
-  });
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: "error occured" });
-  });
-
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server running at port 5000")
   });
